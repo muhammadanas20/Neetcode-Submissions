b6a615fd7b4ff6ee8e1b9473a4928e2fd82299d6
Muhammad Anas <k250899@nu.edu.pk>
2026-07-24 11:14:03 +0500

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        //using selection sort which is inefficient O(n^2) time complexity
+       int n = nums.size();
+        for(int i=0;i<n-1;i++){
+            int min_idx = i;
+
+            for(int j=i+1;j<n;j++){
+                if(nums[j] < nums[min_idx]) min_idx = j;
+            }
+
+            swap(nums[i],nums[min_idx]);
+        }
+    }
+};
